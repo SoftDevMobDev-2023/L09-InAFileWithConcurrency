576345e21abbac04b8b7232251b6d97d1b117b1a
N. A. Ronald <nronald@swin.edu.au>
2023-09-08 12:12:37 +1000

    Added concurrency to long task.

--- app/src/main/java/au/edu/swin/sdmd/l08_inafile_2023/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/l08_inafile_2023/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import au.edu.swin.sdmd.l08_inafile_2023.ui.main.SectionsPagerAdapter
 import au.edu.swin.sdmd.l08_inafile_2023.databinding.ActivityMainBinding
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val model: ButtonViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
